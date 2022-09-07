8d0f15f001f25929c8c748b546bed6a09b83928d
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2022-09-07 15:21:29 -0300

    fix issues

---
+++ src/BugDetector.py
@@ -0,0 +1,52 @@
+import pickle
+from typing import List
+import pandas as pd
+import numpy as np
+
+
+class BugDetector:
+    def __init__(self, data_path, model_path):
+        self.data_path = data_path
+        self.model_path = model_path
+        self.data = self.load_data()
+        self.model = self.load_model()
+        self.examples = []
+
+    def load_model(self):
+        #loads model from path
+        with open(self.model_path, 'rb') as f:
+            return pickle.load(f)
+
+    def load_data(self):
+        #loads data from path
+        return pd.read_csv(self.data_path)
+
+
+    def create_examples(self, columns: List, max_offset = 1.5):
+        #creates examples to be used on other functions
+        pass
+
+    
+    def check_upper_limit_data(self, upperlim, columns: List):
+        pass
+
+    def check_lower_limit_data(self, lowerlim, columns: List):
+        #checks if there is a value lower than the limit in data
+        pass
+
+    def check_specific_values(self, output, values: List):
+        #checks if specific values have specific output both in data and model
+        pass
+
+    def check_monotonic(self, column, positive = True):
+        #checks if feature has monotonic relationship with output (positive or negative)
+        pass
+
+
+    
+
+    
+
+
+
+    

7842734418d3d35c1e5f71be36321b020db9f9bb
William Silva <williamars@al.insper.edu.br>
2022-09-19 16:39:46 -0300

    Sanity test implemented with pytest and parametrize

---
+++ src/test_sanity_check.py
@@ -0,0 +1,34 @@
+import pytest
+import pandas as pd
+import pickle
+
+with open('models/LogisticRegression/LogisticRegression.pkl', 'rb') as f:
+    LR = pickle.load(f)
+with open('models/RandomForest/RandomForest.pkl', 'rb') as f:
+    RF = pickle.load(f)
+
+full_df = pd.read_csv('../datasets/fraud_new.csv')
+
+# Function without pytest
+def sanity_check(model, examples, target_column):
+    result = model.predict(examples.drop(target_column, axis=1))
+    original = examples[target_column]
+    values = (pd.Series(result) == original).value_counts().index
+    if len(values) == 2:
+        return False
+    return values[0]
+
+# Function with pytest
+@pytest.mark.parametrize("model, examples, target_column", [
+    (LR, full_df, "isFraud"),
+    (RF, full_df, "isFraud")
+])
+def test_sanity(model, examples, target_column):
+    result = model.predict(examples.drop(target_column, axis=1))
+    original = examples[target_column]
+    values = (pd.Series(result) == original).value_counts().index
+    if len(values) == 2:
+        assert values[0] == True
+        assert values[1] == True
+    else:
+        assert values[0] == True

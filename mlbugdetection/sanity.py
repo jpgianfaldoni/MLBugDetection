c372fcc95211447c59bf5e723b29bd1be4dc7ecf
William Silva <williamars@al.insper.edu.br>
2022-10-06 11:54:02 -0300

    Initialize the sanity documentation

--- mlbugdetection/sanity.py
+++ mlbugdetection/sanity.py
@@ -1,6 +1,22 @@
 import pandas as pd
 
 def sanity_check(model, examples, target_column):
+    '''
+    Parameters
+    ----------
+    model : sklearn model
+        The model to be used for prediction.
+
+    examples : pandas DataFrame
+        The examples to be used for prediction.
+
+    target_column : str
+        The name of the column containing the target variable.
+
+    Returns
+    -------
+    True if the model is sane, False otherwise.
+    '''
     result = model.predict(examples.drop(target_column, axis=1))
     original = examples[target_column]
     values = (pd.Series(result) == original).value_counts().index

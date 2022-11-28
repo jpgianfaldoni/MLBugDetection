95d1af965811351db4c4fa9c8ef30ea69e8f56e3
Jpgianfaldoni <jpgianfaldoni@gmail.com>
2022-11-28 15:36:49 -0300

    improve tutorial

---
+++ build/lib/mlbugdetection/load_models.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import pkg_resources
+
+
+def load_models_path():
+    """
+        Loads MPLClassifer and the KNN models paths trained with the Breast Cancer classification Dataset
+
+        Returns
+        -------
+        Sklearn MPLClassifier and KNN models
+    """
+    stream_knn = pkg_resources.resource_stream(__name__, 'models/KNNBreastCancer.pkl')
+    stream_nn = pkg_resources.resource_stream(__name__, 'models/NNBreastCancer.pkl')
+    
+    return stream_knn.name.replace("\\", "/"), stream_nn.name.replace("\\", "/")
